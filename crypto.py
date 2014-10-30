864196c95eb8117fac37a1ce21c9f846de824892
Phil Snyder <subfivemile@gmail.com>
2014-10-29 19:46:13 -0700

    Prime Generation and Extended Euclid's Algorithm
    Cleanup + placeholders for future development

--- crypto.py
+++ crypto.py
@@ -10,14 +10,14 @@ import itertools
 
 class SecretMessage(object):
     def __init__(self):
-        pass
+        self.TEST_PRIMES = self.get_primes_erat()
 
     def public_key(self):
         primes = self.generate_primes()
         composite = reduce(lambda x, y : x*y, primes)
         totient = self.generate_totient(primes[0], primes[1])
         exponent = random.sample([x for x in range(3, 100) 
-            if x % 2 and self.wiki_egcd(x, totient)[0] == 1 and x < totient], 1)[0] 
+            if x % 2 and self.egcd(x, totient)[0] == 1 and x < totient], 1)[0] 
         self.decryption_key = self.mod_inverse(exponent, totient)
         self.modulus = composite
         return exponent, composite 
@@ -38,31 +38,14 @@ class SecretMessage(object):
         return (prime1 - 1) * (prime2 - 1)
 
     def mod_inverse(self, e, m):
-        gcd, x, y = self.wiki_egcd(e, m)
+        gcd, x, y = self.egcd(e, m)
         if gcd != 1:
             return ArithmeticError("Modular inverse does not exist")
         else: 
             return x % m
 
-    # extended Euclidean algorithm (WIP)
-    def egcd(self, n1, n2):
-        larger = max(n1, n2)
-        smaller = min(n1, n2)
-        s = [0, 1]
-        t = [1, 0] 
-        r = 1
-        while (r != 0):
-            if (not larger % smaller):
-                r = larger % smaller
-                quotient = larger / smaller
-                s.append(s[-2] - quotient * s[-1])
-                t.append(t[-2] - quotient * t[-1])
-                larger = max(smaller, r)
-                smaller = min(smaller, r)
-            else:
-                return r, s[-1], t[-1]
-
-    def wiki_egcd(self, a, b):
+    # extended Euclidean algorithm 
+    def egcd(self, a, b):
         x,y, u,v = 0,1, 1,0
         while a != 0:
             q, r = b//a, b%a
@@ -71,21 +54,13 @@ class SecretMessage(object):
         gcd = b
         return gcd, x, y
 
-    def wiki_modinv(self, a, m):
-        gcd, x, y = self.wiki_egcd(a, m)
-        if gcd != 1:
-            return None  # modular inverse does not exist
-        else:
-            return x % m
-
     def generate_primes(self):
-        reference_range = random.randrange(10, 100)
-        primes_to_65000 = self.get_primes_erat(65000)
-        potential_primes = [i for i in range(pow(10, 9), pow(10, 9) + pow(10, 3))]
+        # just a few placeholder primes until I can figure out RSA prime guessing/checking
+        potential_primes = [18805410879551, 5250771443623, 3872275671701, 19229337172277]
         primes = set()
         for potential in potential_primes:
             prime = True
-            for i in primes_to_65000:
+            for i in self.TEST_PRIMES:
                 if not potential % i:
                     prime = False
                     break
@@ -108,5 +83,5 @@ class SecretMessage(object):
                     x += p
                 D[x] = p
                 
-    def get_primes_erat(self, n):
-        return list(itertools.takewhile(lambda p: p<n, self.erat2()))
+    def get_primes_erat(self):
+        return list(itertools.takewhile(lambda p: p<65000, self.erat2()))
