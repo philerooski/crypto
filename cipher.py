f400656059b640d6646c26be24d757c507eb4037
Phil Snyder <subfivemile@gmail.com>
2014-12-28 13:01:19 -0800

    One-Time Pad

--- cipher.py
+++ cipher.py
@@ -1,17 +1,31 @@
+import random as r 
+
 class Cipher(object):
     def __init__(self):
         pass
 
     def encrypt(self, message, shift):
         encrypted_message = ''
-        for letter in list(message):
-            index = ord(letter)
-            encrypted_message = encrypted_message + chr(index + shift)
+        for i in range(len(message)):
+            index = ord(message[i])
+            if type(shift) == int:
+                encrypted_message = encrypted_message + chr((index + shift) % 255)
+            elif type(shift) == list:
+                encrypted_message = encrypted_message + chr((index + shift[i]) % 255)
         return encrypted_message
 
     def decrypt(self, ciphertext, shift):
         decrypted_message = ''
-        for letter in list(ciphertext):
-            index = ord(letter)
-            decrypted_message = decrypted_message + chr(index - shift)
+        for i in range(len(ciphertext)):
+            index = ord(ciphertext[i])
+            if type(shift) == int:
+                decrypted_message = decrypted_message + chr((index - shift) % 255)
+            elif type(shift) == list:
+                decrypted_message = decrypted_message + chr((index - shift[i]) % 255)
         return decrypted_message
+
+    def one_time_pad(self, message):
+        pad = []
+        for letter in message:
+            pad.append(r.randint(0, 255))
+        return pad
